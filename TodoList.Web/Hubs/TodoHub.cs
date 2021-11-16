daef5dcf172bc032e4e4eb67d4fe329a40b9782e
sigged <sderdeyn@gmail.com>
2021-11-16 16:30:48 +0100

    Add project files.

---
+++ TodoList.Web/Hubs/TodoHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoList.Web.Domain;
+
+namespace TodoList.Web.Hubs
+{
+    public class TodoHub : Hub
+    {
+
+        public async Task AddTodo(TodoItem todoItem)
+        {
+            await this.Clients.Others.SendAsync("RemoteItemAdded", todoItem);
+        }
+    }
+}
---
+++ TodoList.Web/Services/TodoClient.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR.Client;
+using TodoList.Web.Domain;
+using TodoList.Web.Hubs;
+
+namespace TodoList.Web.Services
+{
+    public class TodoClient
+    {
+        protected HubConnection connection = null;
+        protected Action<TodoItem> callback;
+
+        public TodoClient(Action<TodoItem> callback)
+        {
+            this.callback = callback;
+
+            connection = new HubConnectionBuilder()
+               .WithUrl("https://localhost:5001/todohub")
+               .Build();
+        }
+
+        public async Task Start()
+        {
+            connection.On("RemoteItemAdded", callback);
+
+            await connection.StartAsync();
+        }
+
+        public async Task AddItem(TodoItem newItem)
+        {
+            await connection.SendAsync(nameof(TodoHub.AddTodo), newItem);
+        }
+
+
+    }
+}
